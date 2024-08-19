293ee45b64ee4a0e51745cacbaa45fe408fedfde
Shahan <shahan.khaan@gmail.com>
2024-08-19 19:44:46 +0500

    feat: integrated swagger as a api docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { description, name, version } from '../package.json';
+import { AppModule } from './app.module';
+import { configs } from './config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { logger: ['error', 'warn'] });
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(7000);
+
+	const config = new DocumentBuilder()
+		.setTitle(name)
+		.setDescription(description)
+		.setVersion(version)
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api-docs', app, document, {
+		customCss: '.swagger-ui .topbar { display: none }',
+		customSiteTitle: name,
+		customfavIcon: `${configs.app.baseUrl}/favicon.ico`,
+	});
+
+	await app.listen(configs.app.port, () =>
+		console.log(`Server running on port ${configs.app.port}`),
+	);
 }
 bootstrap();
