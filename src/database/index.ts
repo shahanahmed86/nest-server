0540016abfbaf698e36a204fdb0a2a9861131928
Shahan <shahan.khaan@gmail.com>
2024-08-20 13:46:33 +0500

    fix: run migrations automatically when not in dev env

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { configs } from '../config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 const { dialect, host, port, username, password, database } = configs.db;
-const inProd = configs.app.env === 'production';
+const notInDev = configs.app.env !== 'development';
 
 const typeormConfig: DataSourceOptions = {
 	type: dialect,
@@ -14,7 +14,7 @@ const typeormConfig: DataSourceOptions = {
 	database,
 	entities: [__dirname + '/**/*.entity.{ts,js}'],
 	migrations: [__dirname + '/**/*.migration.{ts,js}'],
-	migrationsRun: inProd,
+	migrationsRun: notInDev,
 	migrationsTransactionMode: 'each',
 	synchronize: false,
 	logging: true,
