352addd745f961026650ccd5158b5a816a46fbd9
Shahan <shahan.khaan@gmail.com>
2024-08-22 18:56:15 +0500

    feat: integrated bcrypt as a library

---
+++ src/library/bcrypt.library.ts
@@ -0,0 +1,8 @@
+import * as bcrypt from 'bcrypt';
+import { configs } from '../config';
+
+const { salt } = configs.bcrypt;
+
+export const hashSync = (str: string) => bcrypt.hashSync(str, salt);
+
+export const compareSync = (str: string, hash: string) => bcrypt.compareSync(str, hash);
--- src/utils/constant.util.ts
+++ src/utils/constant.util.ts
@@ -8,11 +8,6 @@ export const JWT_EXPIRY_IN_SEC = expiry / ONE_SECOND;
 
 export const SIZE_LIMIT = 10 * 1024 * 1024; // 10 MB
 
-export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-
-export const PHONE_REGEX =
-	/^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/;
-
 export const LIMIT = 10;
 
 export const PAGE = 1;
