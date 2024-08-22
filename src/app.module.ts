879674071934d7160749f495076c88077579e9f0
Shahan <shahan.khaan@gmail.com>
2024-08-22 19:18:22 +0500

    revert: moved all the imports back to module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RouterModule } from '@nestjs/core';
+import { GendersModule } from './api/genders/genders.module';
+import { RolesModule } from './api/roles/roles.module';
 import { AppController } from './app.controller';
-import { AllImports } from './app.imports';
 import { AppService } from './app.service';
+import config from './config';
+import redis from './library/redis.library';
 import { CompressionMiddleware } from './middleware/compression.middleware';
 import { CookieParserMiddleware } from './middleware/cookie.middleware';
 import { CorsMiddleware } from './middleware/cors.middleware';
@@ -9,9 +14,26 @@ import { FileUploadMiddleware } from './middleware/fileupload.middleware';
 import { HelmetMiddleware } from './middleware/helmet.middleware';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { RateLimiterMiddleware } from './middleware/rate.middleware';
+import typeorm from './typeorm';
 
 @Module({
-	imports: AllImports,
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			load: [config, typeorm, redis],
+		}),
+		RouterModule.register([
+			{
+				path: 'api',
+				children: [
+					{ path: 'v1/genders', module: GendersModule },
+					{ path: 'v1/roles', module: RolesModule },
+				],
+			},
+		]),
+		GendersModule,
+		RolesModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
