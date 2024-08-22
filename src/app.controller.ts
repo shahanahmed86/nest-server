34bbed67ab6f7c8b6320dd26a086fea90a1cc909
Shahan <shahan.khaan@gmail.com>
2024-08-22 17:43:46 +0500

    docs: added api docs to healthcheck route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller('')
+@Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get('healthcheck')
+	@ApiOkResponse({ type: String, description: 'check health' })
 	healthcheck(): string {
 		return this.appService.healthcheck();
 	}
